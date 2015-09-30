4557bfe573342157393279ab80172580c1957a35
masataka_kawashita <masa.tp@gmail.com>
2015-09-30 14:34:56 +0000

    add want,have change2

--- app/models/user.rb
+++ app/models/user.rb
@@ -44,7 +44,7 @@ class User < ActiveRecord::Base
   end
 
   def have?(item)
-    haves.include?(item)
+    have_items.include?(item)
   end
 
   def want(item)
@@ -56,6 +56,6 @@ class User < ActiveRecord::Base
   end
 
   def want?(item)
-    wants.include?(item)
+    want_items.include?(item)
   end
 end
