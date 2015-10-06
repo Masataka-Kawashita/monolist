2a74b7ae403a700a08bf7741022ebfc068a644b1
masataka_kawashita <masa.tp@gmail.com>
2015-10-06 11:05:02 +0000

    ranking channged

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,9 +1,9 @@
 class RankingController < ApplicationController
     def have
-        @items = Have.select("item_id, count(1) as have_count,items.*").group("item_id").order("have_count DESC").joins("LEFT OUTER JOIN items ON items.id = ownerships.item_id").limit(10)
+        @items = Have.select("item_id, count(1) as have_count,items.*").group("ownerships.item_id").order("have_count DESC").joins("LEFT OUTER JOIN items ON items.id = ownerships.item_id").limit(10)
     end
     
     def want
-       @items = Want.select("item_id, count(1) as want_count,items.*").group("item_id").order("want_count DESC").joins("LEFT OUTER JOIN items ON items.id = ownerships.item_id").limit(10)
+       @items = Want.select("item_id, count(1) as want_count,items.*").group("ownerships.item_id").order("want_count DESC").joins("LEFT OUTER JOIN items ON items.id = ownerships.item_id").limit(10)
     end
 end
